4f58a46f5f3599d733b01940e192ab4ae3a2e930
magn3054 <mdch20@student.aau.dk>
2025-03-10 14:11:31 +0100

    sbhf

--- src/views/DefaultPage.jsx
+++ src/views/DefaultPage.jsx
@@ -4,20 +4,20 @@ import mystyle from "./DefaultPage.module.css";
 export default function DefaultPage() {
 
     const itemlist = [
-        { id: "1", name: "item 1", desc: "item 1 description" },
-        { id: "2", name: "item 2", desc: "item 2 description" },
-        { id: "3", name: "item 3", desc: "item 3 description" },
+        { id: "forward", name: "Gik du kold i går?", desc: "Spørg vores " },
+        { id: "backward", name: "Skal du tage hjem?", desc: "item 2 description" },
     ]
 
     return (
-        <section>
+        <section style={{ border: "red solid 1px" }}>
             <h1>Default page</h1>
             <div className={mystyle.flexcards}>
                 {itemlist.map((item, index) => (
                     <div key={index} className={mystyle.card}>
-                        <h1>{item.name}</h1>
-                        <p>{item.desc}</p>
-                        <p><Link to={`/detail/${item.id}`}>Read more</Link></p>
+                        <Link to={`/${item.id}`}>
+                            <h1>{item.name}</h1>
+                            <p>{item.desc}</p>
+                        </Link>
                     </div>
                 ))}
             </div>
